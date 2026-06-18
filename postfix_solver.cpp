4902ae0d8c6fd4e58716533b7c474227cd7c8d4a
Abinash-25-hue <shivanand1.july.22@gmail.com>
2026-06-18 22:07:51 +0530

    test : added test cases for validity_checker
    Added two more test cases for validity_checker function.

--- postfix_solver.cpp
+++ postfix_solver.cpp
@@ -47,6 +47,8 @@ int main ()
     vector <string> test_1;
     test_1.push_back("(2+3)*4");
     test_1.push_back("(2+3*(4))");
+    test_1.push_back("(2+3*(4)");
+    test_1.push_back("2+3*(4))");
     test_1.push_back("((()(())))");
     test_1.push_back("{}[]()");
     test_1.push_back("{[[]]({}[])}");
