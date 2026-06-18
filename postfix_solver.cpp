4d4f2f224443f9e36dbb19718441f4298e4b5118
Abinash-25-hue <shivanand1.july.22@gmail.com>
2026-06-18 20:51:06 +0530

    feat : added the validity_checker function
    This function checks if the brackets in a mathematical equation are valid.

--- postfix_solver.cpp
+++ postfix_solver.cpp
@@ -2,6 +2,46 @@
 #include<bits/stdc++.h>
 using namespace std;
 
+bool validity_checker (string st)
+{
+    stack <char> s;
+    for (int index = 0; index < st.size(); index++)
+    {
+        if (st[index] == '(' || st[index] == '{' || st[index] == '[')
+        {
+            s.push(st[index]);
+        }
+        if (st[index] == ')' || st[index] == '}' || st[index] == ']')
+        {
+            if (s.empty())
+            {
+                return false;
+            }
+            if (s.top() == '(' && st[index] == ')')
+            {
+                s.pop();
+            }
+            else if (s.top() == '{' && st[index] == '}')
+            {
+                s.pop();
+            }
+            else if (s.top() == '[' && st[index] == ']')
+            {
+                s.pop();
+            }
+            else
+            {
+                return false;
+            }
+        }
+    }
+    if (s.empty())
+    {
+        return true;
+    }
+    return false;
+}
+
 int main ()
 {
     
