1d66d28cd93dd84b823b6632d09018d7fee50971
Abinash-25-hue <shivanand1.july.22@gmail.com>
2026-07-07 23:43:23 +0530

    feat : added postfix_solver constructor
    Added the constructor for postfix_solver class, to initialize the equation string 'eq'.

--- postfix_solver.java
+++ postfix_solver.java
@@ -1,3 +1,15 @@
-public class postfix_solver {
-    
+import java.util.*;
+
+public class postfix_solver 
+{
+    String eq, final = "";
+
+    public postfix_solver (String eq)
+    {
+        this.eq = eq;
+    }
+    public static void main (String[] args)
+    {
+        Scanner sc = new Scanner (system.in);
+    }
 }
