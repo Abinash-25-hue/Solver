d7a22ded97123531c157ad4754dcb997a107daac
Abinash-25-hue <shivanand1.july.22@gmail.com>
2026-07-08 23:45:31 +0530

    feat : added valid_symbol to postfix_solver.java
    Added the valid_symbol function to postfix_solver.java, this function checks if an invalid symbol is present in the equation.

--- postfix_solver.java
+++ postfix_solver.java
@@ -2,12 +2,28 @@ import java.util.*;
 
 public class postfix_solver 
 {
-    String eq, final = "";
+    String eq;
+    String output = new String ("");
 
     public postfix_solver (String eq)
     {
         this.eq = eq;
     }
+
+    public boolean valid_symbols ()
+    {
+        char eq_array[] = eq.toCharArray();
+        for (int index = 0; index < eq_array.length; index++)
+        {
+            char c = eq_array[index];
+            if ((c < '(' && c > '9') || (c == ','))
+            {
+                return false;
+            }
+        }      
+        return true;  
+    }
+
     public static void main (String[] args)
     {
         Scanner sc = new Scanner (system.in);
