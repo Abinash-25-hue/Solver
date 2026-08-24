56f5d3835422bdf7efb81021046e0566205b069f
Abinash <24051454@kiit.ac.in>
2026-08-24 23:45:02 +0530

    Feat : valid_operator in postfix_functions.hpp
    Added the valid_operator function in the "postfix_functions.hpp" header file.

--- postfix_functions.hpp
+++ postfix_functions.hpp
@@ -58,3 +58,76 @@ bool valid_symbol (string st)
     }
     return true;
 }
+
+bool valid_operator (string str)
+{
+    bool was_operator = false;
+    bool was_number = false;
+    bool was_space = false;
+    bool was_bracket_open = false;
+    bool was_bracket_closed = false;
+
+    char c = str[0];
+    for (int index = 0; index < str.size(); index++)
+    {
+        c = str[index];
+        if (c == '+' || c == '*' || c == '/')
+        {
+            if (was_operator == true)
+            {
+                //cout<< c<< "-1, ";
+                return false;
+            }
+            else
+            {
+                //cout<< c<< "-2, ";
+                was_operator = true;
+                was_number = false;
+                was_space = false;
+            }
+        }
+        if (c >= '0' && c <= '9')
+        {
+            if (was_number && was_space && was_operator == false)
+            {
+                //cout<< c<< "-3, ";
+                return false;
+            }
+            if (was_number == false)
+            {
+                //cout<< c<< "-4, ";
+                was_number = true;
+                was_space = false;
+                was_operator = false;
+            }
+        }
+        if (c == ' ')
+        {
+            //cout<< c<< "5, ";
+            was_space = true;
+        }
+        if (c == '(')
+        {
+            was_bracket_open = true;
+            was_number = false;
+            was_space = false;
+            was_operator = false;
+        }
+        if (c == ')')
+        {
+            if (was_operator)
+            {
+                return false;
+            }
+            else
+            {
+                was_bracket_closed = true;
+                was_number = false;
+                was_space = false;
+                was_operator = false;
+            }
+        }
+
+    }
+    return true;
+}
