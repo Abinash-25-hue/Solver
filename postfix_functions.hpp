253ae481c5ffc768d528565554a9d613712a5805
Abinash <24051454@kiit.ac.in>
2026-08-28 23:59:50 +0530

    Feat : precedence_giver to postfix_functions.hpp
    Added precedence_giver function to 'postfix_functions.hpp' file.

--- postfix_functions.hpp
+++ postfix_functions.hpp
@@ -248,3 +248,20 @@ double number_returner (string st, int start)
 
     return final;
 }
+
+int precedence_giver (char symbol)
+{
+    if (symbol == '^')
+    {
+        return 5;
+    }
+    else if (symbol == '*' || symbol == '/')
+    {
+        return 3;
+    }
+    else if (symbol == '+' || symbol == '-')
+    {
+        return 1;
+    }
+    return 0;
+}
